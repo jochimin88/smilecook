34579b219083fa2fc9fafe2c6403b18a9dc3f84e
jochimin88 <201905041@p.uapa.edu.do>
2020-11-15 13:08:51 -0400

    add model and recipe

---
+++ models/recipe.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+recipe_list = []
+
+def get_last_id():
+    if recipe_list:
+        last_recipe = recipe_list[-1]
+    else:
+        return 1
+    return last_recipe[-1]
+
+class Recipe:
+
+    def __init__(self, name, description, num_of_servings, cook_time, directions):
+        self.name = name
+        self.description = description
+        self.num_of_servings = num_of_servings
+        self.cook_time = cook_time
+        self.directions = directions
+        self.is_publish = False
+
+
